afc3f2598ef70a201d2a02fd54dd94be37e35f81
Bernath Monika <bernathm93@gmail.com>
2016-12-06 14:22:47 +0100

    api.service javitva

---
+++ app/script/services/api.service.js
@@ -0,0 +1,33 @@
+(function () {
+    var token = "";
+    var apiService = function ($http) {
+        this.login = function (email, password) {
+            return $http.post('http://localhost:1337/user/login', {
+                    "email": email,
+                    "password": password
+                }, {
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                }).then(function (response) {
+                    token = response.data.token;
+                    console.log(token);
+                });
+        };
+        this.register = function (email, password) {
+            return $http.post('http://localhost:1337/user/register', {
+                "email": email,
+                "password": password
+
+            });
+        };
+        this.getProducts = function () {
+            return $http.get('http://localhost:1337/product', {
+                headers: {
+                    Authorization: 'JWT ' + token
+                }
+            })
+        }
+    };
+    angular.module('app').service('apiService', apiService);
+})();
