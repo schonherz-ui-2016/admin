8e0b112f68eec18fac4f4ea342db56cec4b4f416
Bernath Monika <bernathm93@gmail.com>
2016-12-08 17:14:35 +0100

    this api works with the master

--- app/script/services/api.service.js
+++ app/script/services/api.service.js
@@ -1,13 +1,11 @@
 (function () {
+    var domain='http://localhost:1337';
+    var header= { headers: {
+        Authorization: 'JWT ' + localStorage.getItem('token')
+    }};
     var apiService = function ($http) {
-        var domain = 'http://localhost:1337';
-        var header = {
-            headers: {
-                Authorization: 'JWT ' + localStorage.getItem('token')
-            }
-        };
         this.login = function (email, password) {
-            return $http.post(domain + '/user/login', {
+            return $http.post(domain+'/user/login', {
                 "email": email,
                 "password": password
             }, {
@@ -15,18 +13,17 @@
                     'Content-type': 'application/json'
                 }
             }).then(function (response) {
-                localStorage.setItem('token', response.data.token) ;
+                localStorage.setItem('token', response.data.token);
             });
         };
         this.register = function (email, password) {
-            return $http.post(domain + '/user/register', {
+            return $http.post(domain+'/user/register', {
                 "email": email,
                 "password": password
-
             }, header);
         };
         this.getProducts = function () {
-            return $http.get(domain + '/product', header)
+            return $http.get(domain+'/product', header);
         }
     };
     angular.module('app').service('apiService', apiService);
