2d130da45df8d8d5696ca26915dd308f914f47ff
Nagy Norbert <nagynoorbert12@gmail.com>
2016-12-12 18:05:07 +0100

    register Object

--- app/script/services/api.service.js
+++ app/script/services/api.service.js
@@ -1,28 +1,26 @@
 (function () {
 
     var domain='http://localhost:1337';
-    var header= { headers: {
-        Authorization: 'JWT ' + localStorage.getItem('token')
-    }};
-
     var apiService = function ($http) {
 
         this.login = function (email, password) {
             return $http.post(domain+'/user/login', {
                 "email": email,
                 "password": password
-            }, {
-                headers: {
-                    'Content-type': 'application/json'
-                }
             }).then(function (response) {
                 localStorage.setItem('token', response.data.token);
             });
         };
-        this.register = function (email, password) {
+        this.register = function (user) {
             return $http.post(domain+'/user/register', {
-                "email": email,
-                "password": password
+                "email": user.email,
+                "password": user.password,
+                "name":user.name,
+                "address":user.address,
+                "phone":user.phone,
+                "balance":user.balance,
+                "rights":user.rights
+
             });
         };
         this.getProducts = function () {
