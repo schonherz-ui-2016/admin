bd4c4eaee661c4e499e0b5c98766050eb6152b0f
kovacssandorUI <sandorkovacs122@gmail.com>
2016-12-20 14:01:24 +0100

    delete_stringToken

--- app/script/controllers/main.controller.js
+++ app/script/controllers/main.controller.js
@@ -4,13 +4,11 @@
 (function () {
     angular.module('app')
         .controller('mainController', function (apiService, $scope) {
-            var stringToken="";
             $scope.$watch(function () {
                 return localStorage.getItem('token');
             }, function (token) {
                 $scope.token=token;
-                stringToken=token;
-                if(stringToken!==""){
+                if(token){
                     apiService.getUserId()
                         .then(function (res) {
                         $scope.user=res.data;
