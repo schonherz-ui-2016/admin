17d53481eee406841d0ba9049bc3067a4dfdc9b1
kovacssandorUI <sandorkovacs122@gmail.com>
2016-12-21 09:45:04 +0100

    fixed

--- app/script/controllers/main.controller.js
+++ app/script/controllers/main.controller.js
@@ -4,22 +4,15 @@
 (function () {
     angular.module('app')
         .controller('mainController', function (apiService, $scope) {
-            var stringToken="";
             localStorage.setItem('loggedIn', false);
             $scope.$watch(function () {
                 return localStorage.getItem('token');
             }, function (token) {
                 $scope.token=token;
-                stringToken=token;
-                var id="";
-                if(stringToken!==""){
+                if(token){
                     apiService.getUserId()
-                        .then(function (res) {
-                        id=res.data;
-                            apiService.getUser(id)
-                                .then(function (response) {
-                                    $scope.user=response.data;
-                                })
+                        .then(function (response) {
+                            $scope.user=response.data;
                         })
                 }
                });
