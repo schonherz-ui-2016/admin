5734d18a49a7b993cad2a08e0dd4e9dca611e778
Bernath Monika <bernathm93@gmail.com>
2017-01-03 12:47:01 +0100

    fixed link

---
+++ app/script/directives/sortable.list/sortable.list.directive.js
@@ -0,0 +1,22 @@
+(function () {
+    angular.module('app')
+        .directive('sortableListDirective', function () {
+            return {
+                restrict: 'A',
+                scope: {
+                    data: '=',
+                    link: '='
+                },
+                controller: function ($scope) {
+                    $scope.attributes = Object.keys($scope.data[0]);
+                    $scope.propertyName = $scope.attributes[0];
+                    $scope.reverse = false;
+                    $scope.sortBy = function (propertyName) {
+                        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+                        $scope.propertyName = propertyName;
+                    };
+                },
+                templateUrl: 'script/directives/sortable.list/table.html'
+            };
+        })
+})();
