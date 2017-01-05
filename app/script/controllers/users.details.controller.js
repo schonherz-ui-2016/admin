fc030864d8529cfbc459295d0a35b104da5de030
Nagy Norbert <nagynoorbert12@gmail.com>
2017-01-05 13:11:20 +0100

    user update finished

--- app/script/controllers/users.details.controller.js
+++ app/script/controllers/users.details.controller.js
@@ -1,11 +1,13 @@
 
 (function () {
     angular.module('app')
-        .controller('usersDetailsController', function (apiService, $scope, $routeParams) {
+        .controller('usersDetailsController', function (apiService, $scope, $routeParams, $location) {
             apiService.getUser($routeParams.id)
                 .then(function (res) {
                     $scope.users = res.data;
                 });
-
+            $scope.updateUser = function () {
+                $location.path("/userUpdate/"+$routeParams.id);
+            }
         })
 })();
