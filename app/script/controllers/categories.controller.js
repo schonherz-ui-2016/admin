d52c13e78552ca67f73b73459aa960d0115ef79a
Bernath Monika <bernathm93@gmail.com>
2017-01-06 14:58:14 +0100

    version1.1

--- app/script/controllers/categories.controller.js
+++ app/script/controllers/categories.controller.js
@@ -3,8 +3,28 @@
         .controller('categoriesController', function ($http, $scope, $locale, apiService, $location) {
             apiService.getCategories()
                 .then(function (res) {
-                    console.log(res);
                     $scope.categories = [];
+                    $scope.allCategories = [
+                        {
+                            name: 'Mezőgazdasági eszközök',
+                            children: [
+                                {
+                                    name: 'Ekék',
+                                    children: [
+                                        {
+                                            name: 'Kiseke'
+                                        },
+                                        {
+                                            name: 'Nagyeke'
+                                        }
+                                    ]
+                                },
+                                {
+                                    name: 'Traktorok'
+                                }
+                            ]
+                        }
+                    ];
                     angular.forEach(res.data, function (x) {
                         var obj = {id: x.id, name: x.name, parent: x.parent, createdAt: x.createdAt, updatedAt: x.updatedAt};
                         if(obj.parent != null){
@@ -12,7 +32,6 @@
                         }
                         $scope.categories.push(obj);
                     });
-                    console.log($scope.categories);
                 });
         });
 })();
