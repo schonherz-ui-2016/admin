8935ce83c0e901b5c1e3b9640938eac2ba06928e
Bernath Monika <bernathm93@gmail.com>
2017-01-10 14:23:33 +0100

    remove button off the last child

--- app/script/directives/categories.tree/category.tree.directive.js
+++ app/script/directives/categories.tree/category.tree.directive.js
@@ -9,7 +9,19 @@
                 scope: {
                     data:'='
                 },
-                templateUrl:'script/directives/categories.tree/category.tree.html'
+                templateUrl:'script/directives/categories.tree/category.tree.html',
+                controller: function ($scope) {
+                    $scope.openfv = function (id) {
+                        console.log(id);
+                        angular.forEach($scope.data, function (x) {
+                            if(x.id == id){
+                                if(x.children.length == 0){
+                                    $('.sign').remove();
+                                }
+                            }
+                        })
+                    }
+                }
             };
         })
 })();
