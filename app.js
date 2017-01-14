d084a7dc30c184498a9af16eb22fd58a08bcb38f
kovacssandorUI <sandorkovacs122@gmail.com>
2017-01-15 00:39:33 +0100

    add_acces_controll_origin

--- app.js
+++ app.js
@@ -8,9 +8,15 @@ var express = require('express'),
 
 app.use(express.static(__dirname + '/app'));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/app/index.html');
 });
-app.listen(3000, '81.2.254.9', function () {
+app.listen(3000,  function () {
     console.log("Server has started on 3000 port!!");
 });
