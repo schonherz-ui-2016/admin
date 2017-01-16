30c24751eff76a101aeead665e2fb091e2d40494
kovacssandorUI <sandorkovacs122@gmail.com>
2017-01-16 23:03:11 +0100

    start:PORT=8000_node_app.js

--- app.js
+++ app.js
@@ -3,20 +3,17 @@
  */
 var express = require('express'),
     app = express(),
-    http = require('http'),
-    httpServer = http.Server(app);
+    http = require('http');
+
 
 app.use(express.static(__dirname + '/app'));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res) {
     res.sendfile(__dirname + '/app/index.html');
 });
-app.listen(3000,  function () {
-    console.log("Server has started on 3000 port!!");
+app.listen(port,  function () {
+    console.log("Server has started on "+port +" port!!");
 });
