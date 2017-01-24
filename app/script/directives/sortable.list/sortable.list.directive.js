823fae15c78568d3f2aa21321ccd6bab03d6f8af
kovacssandorUI <sandorkovacs122@gmail.com>
2017-01-24 14:20:25 +0100

    +files with dependency injection,it working!

--- app/script/directives/sortable.list/sortable.list.directive.js
+++ app/script/directives/sortable.list/sortable.list.directive.js
@@ -8,16 +8,17 @@
                     link: '=',
                     search: '='
                 },
-                controller: function ($scope) {
-                    $scope.attributes = Object.keys($scope.data[0]);
-                    $scope.propertyName = $scope.attributes[0];
-                    $scope.reverse = false;
-                    $scope.sortBy = function (propertyName) {
-                        $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
-                        $scope.propertyName = propertyName;
-                    };
-                },
+                controller: 'sortableListController',
                 templateUrl: 'script/directives/sortable.list/table.html'
             };
         })
+        .controller('sortableListController',['$scope', function ($scope) {
+            $scope.attributes = Object.keys($scope.data[0]);
+            $scope.propertyName = $scope.attributes[0];
+            $scope.reverse = false;
+            $scope.sortBy = function (propertyName) {
+                $scope.reverse = ($scope.propertyName === propertyName) ? !$scope.reverse : false;
+                $scope.propertyName = propertyName;
+            };
+        }])
 })();
